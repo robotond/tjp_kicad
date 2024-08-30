7710cbdffdcc12a499c4bd857f372f2193966fdf
Janos Peter Toth <tojape@gmail.com>
2024-08-30 06:26:45 +0200

    some parts added

---
+++ 3dmodels/TJP_3D_models/5000/PADS/5000.d
@@ -0,0 +1,25 @@
+*PADS-LIBRARY-PCB-DECALS-V9*
+
+5000             M -0.759 -0.086 0 2 3 0 1 1 0
+TIMESTAMP 2017.10.10.13.15.49
+-0.392 1.99  0     0 0.381 0.0381 1 0 34 "Regular <Romansim Stroke Font>"
+REF-DES
+-0.392 1.99  0     0 0.381 0.0381 1 32 35 "Regular <Romansim Stroke Font>"
+PART-TYPE
+CIRCLE 2 0.2 27 -1
+1.27  0    
+-1.27 0    
+CIRCLE 2 0.2 27 -1
+1.27  0    
+-1.27 0    
+CIRCLE 2 0.2 26 -1
+1.27  0    
+-1.27 0    
+T0     0     0     0     1
+PAD 0 3 P 1.02 
+-2 1.6 R  
+-1 1.6 R  
+0  1.6 R
+*END*
+*REMARK* AP
+5000.stp/90/0/0/0/0/1.7100000381469727
---
+++ 3dmodels/TJP_3D_models/5000/PADS/5000.p
@@ -0,0 +1,17 @@
+*PADS-LIBRARY-PART-TYPES-V9*
+
+5000 5000 I CON 6 1 0 0 0
+TIMESTAMP 2024.08.29.19.52.45
+"Farnell Part Number" 
+"Farnell Price/Stock" 
+"Manufacturer_Name" Keystone Electronics
+"Manufacturer_Part_Number" 5000
+"Description" KEYSTONE - 5000 - TEST POINT, CONNECTOR, RED
+"Datasheet Link" http://www.keyelco.com/product-pdf.cfm?p=1309
+GATE 1 1 0
+5000
+1 0 U 1
+
+*END*
+*REMARK* SamacSys ECAD Model
+293483/1120615/2.50/1/3/Connector
